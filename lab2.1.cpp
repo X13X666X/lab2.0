c92f3e43ee9f786db0aca9c3ea5e856d297f99d9
Max Chaplia <82676425+X13X666X@users.noreply.github.com>
2024-10-07 05:17:42 +0300

    Update lab2.1.cpp
    закоментували кожен рядок який містив z2

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -13,10 +13,10 @@ int main()
 	//double z2; // ðåçóëüòàò îá÷èñëåííÿ 2-ãî âèðàçó
 	cout << "a = "; cin >> a;
 	z1 = (cos(a)+sin(a))/(cos(a)-sin(a));
-	z2 = tan(2*a)+1/cos(2*a);
+	//z2 = tan(2*a)+1/cos(2*a);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
